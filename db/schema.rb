93946e4eafc02e05a157cbe45a11a132ee718a5e
Daniel Leal <dlleal@uc.cl>
2019-04-17 10:17:05 -0400

    Changed database schema seed to first date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190414174238) do
 
   # These are extensions that must be enabled in order to support this database
@@ -46,7 +45,7 @@ ActiveRecord::Schema.define(version: 20190414174238) do
   create_table "venues", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.text "description"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
