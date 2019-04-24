df1d63c12e1a76a437b9e20b0a0b1f2f67d4e75f
Daniel Leal <dlleal@uc.cl>
2019-04-24 18:09:24 -0400

    Fixed database issue when pushing to production. Precompiled a bunch of css and js assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190414174238) do
+ActiveRecord::Schema.define(version: 20190422142102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20190414174238) do
     t.string "initials"
     t.string "name"
     t.string "teacher_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.text "description"
+    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
