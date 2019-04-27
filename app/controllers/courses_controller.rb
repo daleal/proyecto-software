91c5cc1d70e8fe6fb19839ad840f9208c10ae4de
Daniel Leal <37158127+Panckx@users.noreply.github.com>
2019-04-27 12:29:42 -0400

    Polish courses (#7)
    * Refined controller actions
    
    * Finished polishing courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,8 +12,14 @@ class CoursesController < ApplicationController
   end
 
   def create
-    @course = Course.new
-    @course.save
+    @course = Course.new(course_params)
+    if @course.save
+      flash[:success] = "Se ha creado un ramo con el nombre "
+                      "#{@course.name} correctamente!"
+    else
+      flash[:warning] = "No se ha podido crear el ramo!"
+    end
+    redirect_to courses_path
   end
 
   def edit
@@ -22,13 +28,26 @@ class CoursesController < ApplicationController
 
   def update
     @course = Course.find(params[:id])
-    if @course.update_attributres
-      # Hacer algo
+    if @course.update_attributes(course_params)
+      flash[:success] = "Se ha editado el ramo con el nombre "
+                      "#{@course.name} correctamente!"
+    else
+      flash[:warning] = "No se ha podido editar el ramo!"
     end
+    redirect_to courses_path
   end
 
   def destroy
     @course = Course.find(params[:id])
     @course.destroy
+    flash[:info] = "Se ha eliminado un ramo correctamente"
+    redirect_to courses_path
   end
+
+  private
+
+    def course_params
+      data = params.require(:course).permit(:name, :initials, :teacher_name)
+    end
+
 end
