667d3864711ab867ee17beac945ea25ca04df0dc
Daniel Leal <dlleal@uc.cl>
2019-04-27 19:51:07 -0400

    Now every controller of the app asks for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190427225415) do
+ActiveRecord::Schema.define(version: 20190409163759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20190427225415) do
   end
 
   create_table "rooms", force: :cascade do |t|
+    t.string "letter"
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
