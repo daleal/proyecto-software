aab44f1125c7f7ea6c32bbfb8568566199437004
Daniel Leal <dlleal@uc.cl>
2019-04-27 17:15:59 -0400

    Fixed schema inconsistency

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20190427201757) do
+ActiveRecord::Schema.define(version: 20190409163759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
