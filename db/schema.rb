e9ebc031fe02ab2cb4fa1c5507a4f42896e03f4c
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-05-18 13:19:38 -0400

    modity devise_create_users migration and Users table (#18)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190409163759) do
+ActiveRecord::Schema.define(version: 20190427225415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20190409163759) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string "letter"
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -62,7 +61,6 @@ ActiveRecord::Schema.define(version: 20190409163759) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
