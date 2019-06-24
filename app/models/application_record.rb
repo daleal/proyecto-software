b71c93d28751a9351ab32bff47bf68a15623cff5
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-06-24 17:25:42 -0400

    Search substrings
    * add include_substring search to abstract model class
    
    * fix scope bug
    
    * match substrings (regex) if makes sense to do so
    
    * fix typo;
    
    * replace plus (+) for union operator (|) when catalogging
    
    * fix typo
    
    * update search criteria

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  
+
+  scope :includes_substring, -> (column_name, substring) do
+    where (["((#{self.table_name}.#{column_name} LIKE ?) OR (#{self.table_name}.#{column_name} LIKE ?) OR (#{self.table_name}.#{column_name} LIKE ?) OR (#{self.table_name}.#{column_name} LIKE ?))",
+    "%#{substring.upcase}%", "%#{substring.downcase}%", "%#{substring.capitalize}%", "%#{substring.titleize}%"])
+  end
+
 end
